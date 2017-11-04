cf62d3a31cd56b99247ae32f0de29537cbe458df
Christopher E. Thomas <cthomas@admt.im>
2017-11-04 16:28:02 -0600

    Fix turn manager missing include.

--- src/main/java/im/admt/team11/PA3/Game/TurnManager.java
+++ src/main/java/im/admt/team11/PA3/Game/TurnManager.java
@@ -3,6 +3,8 @@ package im.admt.team11.PA3.Game;
 import im.admt.team11.PA3.Game.Board.Pieces.Die;
 import im.admt.team11.PA3.Game.Board.Pieces.Token;
 
+import java.util.ArrayList;
+
 public class TurnManager{
 
     public int numPlayers;
