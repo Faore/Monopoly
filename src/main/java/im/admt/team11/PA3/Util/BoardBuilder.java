5f94c05e61803e1f633cdf60e3f3f9a7b55009d1
SeanJungst <seanjungst@gmail.com>
2017-11-07 23:45:01 -0700

    Added the rest of the tile coordinates and their orientation
    All tiles now have coordinates and orientation assigned

--- src/main/java/im/admt/team11/PA3/Util/BoardBuilder.java
+++ src/main/java/im/admt/team11/PA3/Util/BoardBuilder.java
@@ -166,6 +166,144 @@ public class BoardBuilder {
                         SpecialTileTypes.FreeParking
                 )
         );
+        tiles.add(
+                new StandardProperty("Kentucky Avenue",
+                        new Point2D(423,410),
+                        new Point2D(652,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new SpecialTile("Chance",
+                        new Point2D(665,410),
+                        new Point2D(890,9),
+                        TileOrientation.Top,
+                        SpecialTileTypes.Chance
+                )
+        );
+        tiles.add(
+                new StandardProperty("Indiana Avenue",
+                        new Point2D(905,410),
+                        new Point2D(1130,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new StandardProperty("Illinois Avenue",
+                        new Point2D(1145,410),
+                        new Point2D(1374,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new StandardProperty("B. & O. Railroad",
+                        new Point2D(1385,410),
+                        new Point2D(1612,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new StandardProperty("Atlantic Avenue",
+                        new Point2D(1625,410),
+                        new Point2D(1850,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new StandardProperty("Ventnor Avenue",
+                        new Point2D(1865,410),
+                        new Point2D(2090,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new StandardProperty("Water Works",
+                        new Point2D(2107,410),
+                        new Point2D(2334,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new StandardProperty("Marvin Gardens",
+                        new Point2D(2345,410),
+                        new Point2D(2574,9),
+                        TileOrientation.Top
+                )
+        );
+        tiles.add(
+                new SpecialTile("Go To Jail",
+                        new Point2D(2587,410),
+                        new Point2D(2988,9),
+                        TileOrientation.Corner,
+                        SpecialTileTypes.GoToJail
+                )
+        );
+        tiles.add(
+                new StandardProperty("Pacific Avenue",
+                        new Point2D(2585,650),
+                        new Point2D(2990,423),
+                        TileOrientation.Right
+                )
+        );
+        tiles.add(
+                new StandardProperty("North Carolina Avenue",
+                        new Point2D(2585,892),
+                        new Point2D(2990,665),
+                        TileOrientation.Right
+                )
+        );
+        tiles.add(
+                new SpecialTile("Community Chest",
+                        new Point2D(2585,1132),
+                        new Point2D(2990,905),
+                        TileOrientation.Right,
+                        SpecialTileTypes.CommunityChest
+                )
+        );
+        tiles.add(
+                new StandardProperty("Pennsylvania Avenue",
+                        new Point2D(2585,1368),
+                        new Point2D(2990,1147),
+                        TileOrientation.Right
+                )
+        );
+        tiles.add(
+                new StandardProperty("Short Line",
+                        new Point2D(2585,1612),
+                        new Point2D(2990,1387),
+                        TileOrientation.Right
+                )
+        );
+        tiles.add(
+                new SpecialTile("Chance",
+                        new Point2D(2585,1852),
+                        new Point2D(2990,1625),
+                        TileOrientation.Right,
+                        SpecialTileTypes.Chance
+                )
+        );
+        tiles.add(
+                new StandardProperty("Park Place",
+                        new Point2D(2585,2090),
+                        new Point2D(2990,1865),
+                        TileOrientation.Right
+                )
+        );
+        tiles.add(
+                new SpecialTile("Luxury Tax",
+                        new Point2D(2585,2330),
+                        new Point2D(2990,2109),
+                        TileOrientation.Right,
+                        SpecialTileTypes.LuxuryTax
+                )
+        );
+        tiles.add(
+                new StandardProperty("Boardwalk",
+                        new Point2D(2585,2572),
+                        new Point2D(2990,2349),
+                        TileOrientation.Right
+                )
+        );
         return tiles;
     }
 }
