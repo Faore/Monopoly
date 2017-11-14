ac4cbc56dafeda9bc17a69a9aaafe0ec8d4dbf2a
Chris Thomas <cthomas@admt.im>
2017-11-14 02:37:24 -0700

    UML Diagram updates.

--- src/test/java/im/admt/team11/PA3/Game/Board/Pieces/TokenTest.java
+++ src/test/java/im/admt/team11/PA3/Game/Board/Pieces/TokenTest.java
@@ -4,10 +4,14 @@ import im.admt.team11.PA3.Game.Player;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
+import static org.junit.Assert.assertTrue;
+
 public class TokenTest extends ApplicationTest {
     @Test
     public void testInitialize() {
         Player player = new Player(1, TokenTypes.Blue);
         Token token = new Token(TokenTypes.Blue, player);
+        assertTrue(player.playerNumber == 1);
+        assertTrue(token.player.playerNumber == 1);
     }
 }
