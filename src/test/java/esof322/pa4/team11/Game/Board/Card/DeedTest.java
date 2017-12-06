5c3a6eaa7aa6e4dee04a6b4ec6d9e449b6b8f81f
tkruegs <tkruegs@gmail.com>
2017-12-05 17:55:22 -0700

    more deed tests

--- src/test/java/esof322/pa4/team11/Game/Board/Card/DeedTest.java
+++ src/test/java/esof322/pa4/team11/Game/Board/Card/DeedTest.java
@@ -1,17 +1,35 @@
 package esof322.pa4.team11.Game.Board.Card;
 
+import esof322.pa4.team11.Game.Board.Pieces.TokenTypes;
+import esof322.pa4.team11.Game.Board.Tiles.Properties.StandardProperty;
+import esof322.pa4.team11.Game.Board.Tiles.Property;
+import esof322.pa4.team11.Game.Board.Tiles.TileOrientation;
+import esof322.pa4.team11.Game.Player;
 import esof322.pa4.team11.Game.UI.GameWindowController;
+import javafx.geometry.Point2D;
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-public class DeedTest {
+public class DeedTest extends ApplicationTest{
 
     //Create test data
     private Deed testDeedBoardwalk;
 
     public DeedTest() throws Exception {
         testDeedBoardwalk = new Deed(400, 50, 200, 200, 600, 1400, 1700, 200);
+    }
+
+    @Test
+    public void testInitialize() throws Exception {
+        Deed railroad = new Deed(200, 25);
+        Deed utility = new Deed(150);
+        assertTrue(testDeedBoardwalk instanceof Deed);
+        assertTrue(railroad instanceof Deed);
+        assertTrue(utility instanceof Deed);
     }
 
     @Test
@@ -22,7 +40,6 @@ public class DeedTest {
 
     @Test
     public void testGetOwner() throws Exception {
-
         assertTrue(null == testDeedBoardwalk.currentOwner);
     }
 
@@ -31,4 +48,52 @@ public class DeedTest {
         assertEquals(0, testDeedBoardwalk.getCurrentBuildingLevel());
     }
 
+    @Test
+    public void testSetOwner() throws Exception {
+        Player player = new Player(1, TokenTypes.Blue);
+        testDeedBoardwalk.setOwner(player);
+        assertEquals(player, testDeedBoardwalk.currentOwner);
+    }
+
+    @Test
+    public void testSetMortgaged() throws Exception {
+        testDeedBoardwalk.setOwner(new Player(1, TokenTypes.Blue));
+        testDeedBoardwalk.setMortgaged(true);
+        assertEquals(true, testDeedBoardwalk.isMortgaged);
+    }
+
+    @Test
+    public void testAssociateWithDeed() throws Exception {
+        Deed testDeedParkPlace = new Deed(350, 35, 200, 175,500,1100,1300, 1500);
+        testDeedBoardwalk.associatedDeeds.add(testDeedParkPlace);
+        testDeedParkPlace.associatedDeeds.add(testDeedBoardwalk);
+        assertTrue(testDeedBoardwalk.associatedDeeds.get(0) == testDeedParkPlace);
+        assertTrue(testDeedParkPlace.associatedDeeds.get(0) == testDeedBoardwalk);
+    }
+
+    @Test
+    public void testAttachToProperty() throws Exception {
+        Property boardwalk = new StandardProperty("Mediterranean Avenue",
+                new Point2D(2344, 2993),
+                new Point2D(2577, 2585),
+                TileOrientation.Bottom, testDeedBoardwalk);
+        testDeedBoardwalk.attachToProperty(boardwalk);
+        assertEquals(boardwalk, testDeedBoardwalk.getProperty());
+    }
+
+    @Test
+    public void testGetProperty() throws Exception {
+        Property boardwalk = new StandardProperty("Mediterranean Avenue",
+                new Point2D(2344, 2993),
+                new Point2D(2577, 2585),
+                TileOrientation.Bottom, testDeedBoardwalk);
+        assertTrue(boardwalk == testDeedBoardwalk.getProperty());
+    }
+
+    @Test
+    public void testAddAndRemoveBuildingLevelOnStandard() throws Exception {
+        testDeedBoardwalk.addBuildingLevel();
+        testDeedBoardwalk.removeBuildingLevel();
+        assertEquals(0, testDeedBoardwalk.getCurrentBuildingLevel());
+    }
 }
